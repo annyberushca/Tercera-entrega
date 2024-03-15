82b87d5482eaea5bfe8f413d4059199fc3141a85
Anny <berushanny@hotmail.com>
2024-03-15 00:49:00 -0500

    version tablas y enlaces

--- precio/aplicacion/views.py
+++ precio/aplicacion/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, "aplicacion/index.html")
 
 def insumos(request):
-
-    return render(request, "aplicacion/insumos.html")
+    contexto = {'insumos':Insumo.objects.all()}
+    return render(request, "aplicacion/insumos.html",contexto)
 
 def personales(request):
     return render(request, "aplicacion/personales.html")
